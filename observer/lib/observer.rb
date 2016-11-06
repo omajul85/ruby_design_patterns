3fe73d58a80d9a67b677be55fce4f861e67c0614
Omar <omajul2@hotmail.com>
2016-11-06 18:15:04 +0000

    Observer pattern: First approach

--- observer/lib/observer.rb
+++ observer/lib/observer.rb
@@ -1,12 +1,29 @@
 # The Observer pattern
 
 class Employee
-  attr_reader :name
-  attr_accessor :title, :salary
+  attr_reader :title, :salary, :name
 
-  def initialize(name, title, salary)
+  def initialize(name, title, salary, payroll)
     @name = name
     @title = title
     @salary = salary
+    @payroll = payroll
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    @payroll.update(self)
   end
 end
+
+class Payroll
+  def update(changed_employee)
+    puts("Cut a new check for #{changed_employee.name}!")
+    puts("His salary is now #{changed_employee.salary}!")
+  end
+end
+
+payroll = Payroll.new
+fred = Employee.new('Fred', 'Crane Operator', 30000, payroll)
+# The payroll department will know all about it
+fred.salary = 35000
